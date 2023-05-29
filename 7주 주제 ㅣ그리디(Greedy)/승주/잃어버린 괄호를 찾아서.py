921c879094f66690415745184bc8ec5fafb02acc
Seungju_Park <134610995+Zoe3399@users.noreply.github.com>
2023-05-29 09:21:56 +0900

    커밋

--- 7주 주제 ㅣ그리디(Greedy)/승주/잃어버린 괄호를 찾아서.py
+++ 7주 주제 ㅣ그리디(Greedy)/승주/잃어버린 괄호를 찾아서.py
@@ -1,14 +1,33 @@
+a = input().split('-')
+res = sum(map(int,a[0].split('+')))
+num = [sum(map(int,i.split('+'))) for i in a[1:]]
+for i in num: res-=i
+print(res)   
+
+''' 
+a = input().split('-')
+res = sum(map(int,a[0].split('+')))
+for i in a[1:]:
+    num = sum(map(int,i.split('+')))
+    res -= num
+print(res)   
+
+[ 코드 더 간단하게 만들기 ]
 # 문자열 입력받기
 a = input().split('-')
-res = sum(list(map(int,a[0].split('+'))))
+# a[0] 먼저 res에 대입
+res = sum(map(int,a[0].split('+')))
+# a[1] 부터 진행
 for i in a[1:]:
-    num = sum(list(map(int,i.split('+'))))
+    # i값에서 '+' 기준으로 나눠서 int형으로 형변환 후 총합
+    num = sum(map(int,i.split('+')))
+    # 차례대로 뺄셈 진행
     res -= num
 print(res)    
 
 
-''' 
 # 문자열 입력받기
+[ 처음 작성한 코드 ]
 a = input().split('-')
 number = []
 for i in a:
